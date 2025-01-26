2133d83137a6b6d785cb1e27c093ea8d0ef47d99
Emlon <901228@gmail.com>
2025-01-26 15:50:24 +0800

    integrate tmux config into wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
-local wezterm = require('wezterm')
+local wezterm = require('wezterm') --[[@as Wezterm]]
 
+---@type Config
+---@diagnostic disable-next-line: missing-fields
 local config = {}
 
 --- Font ---
@@ -28,11 +30,20 @@ config.anti_alias_custom_block_glyphs = true
 config.log_unknown_escape_sequences = true
 config.normalize_output_to_unicode_nfc = true
 
---- Colors ---
-config.colors = {
-    foreground = '#C0C0C0',
-    background = '#000000',
+--- colors ---
+-- local function get_color_scheme()
+--     if wezterm.gui.get_appearance():find('Dark') then
+--         return 'Catppuccin Mocha'
+--     else
+--         return 'Catppuccin Latte'
+--     end
+-- end
+-- config.color_scheme = get_color_scheme()
+config.color_scheme = 'Catppuccin Mocha'
 
+local tab_bar_bg = '#11111b'
+---@diagnostic disable-next-line: missing-fields
+config.colors = {
     cursor_bg = '#1E90FF',
     cursor_fg = '#323232',
     cursor_border = '#40DB85',
@@ -65,23 +76,33 @@ config.colors = {
         '#FFFFFF',
     },
 
-    -- indexed = { [136] = '#af8700' },
-
-    -- copy_mode_active_highlight_bg = { Color = '#000000' },
-
-    -- copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
-    -- copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
-    -- copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
-
-    -- quick_select_label_bg = { Color = 'peru' },
-    -- quick_select_label_fg = { Color = '#ffffff' },
-    -- quick_select_match_bg = { AnsiColor = 'Navy' },
-    -- quick_select_match_fg = { Color = '#ffffff' },compose_cursor = 'orange',
+    tab_bar = {
+        new_tab = {
+            bg_color = tab_bar_bg,
+            fg_color = 'White',
+        },
+        new_tab_hover = {
+            bg_color = 'Grey',
+            fg_color = tab_bar_bg,
+        },
+    },
 }
 -- windows
-config.window_background_opacity = 0.7
--- config.window_decorations = 'INTEGRATED_BUTTONS | RESIZE'
-config.window_decorations = 'TITLE | RESIZE'
+config.window_background_opacity = 0.75
+---@diagnostic disable-next-line: missing-fields
+config.window_frame = {
+    ---@diagnostic disable-next-line: missing-fields
+    font = wezterm.font({ family = 'Roboto', weight = 'Regular' }),
+    font_size = 12.0,
+}
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = '0.5cell',
+    bottom = 0,
+}
+
+config = require('tab_bar').setup(wezterm, config, tab_bar_bg)
 
 -- initial window size
 config.initial_cols = 110
@@ -89,13 +110,21 @@ config.initial_rows = 32
 
 -- plus tab menu entry
 config.launch_menu = {
+    ---@diagnostic disable-next-line: missing-fields
     {
+        label = 'Zsh',
+        args = { 'zsh', '-l' },
+    },
+    ---@diagnostic disable-next-line: missing-fields
+    {
+        label = 'Btop',
         args = { 'btop' },
     },
+    ---@diagnostic disable-next-line: missing-fields
     {
         label = 'Bash',
         args = { 'bash', '-l' },
-    }
+    },
 }
 
 -- front_end
@@ -105,8 +134,136 @@ config.webgpu_power_preference = 'HighPerformance'
 --- Others ---
 config.enable_scroll_bar = true
 config.enable_wayland = false
+
+---@diagnostic disable-next-line: assign-type-mismatch
 config.quote_dropped_files = 'Posix'
 -- config.term = 'wezterm'
 
-return config
+--- Key bindings ---
+local act = wezterm.action
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+    -- command palette
+    {
+        key = 'P',
+        mods = 'CTRL',
+        action = 'ActivateCommandPalette',
+    },
 
+    -- splitting
+    {
+        key = '|',
+        mods = 'LEADER|SHIFT',
+        action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+    },
+    {
+        key = '-',
+        mods = 'LEADER',
+        action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
+    },
+
+    -- change tab
+    {
+        key = 'l',
+        mods = 'LEADER',
+        action = act.ActivateTabRelative(1),
+    },
+    {
+        key = 'h',
+        mods = 'LEADER',
+        action = act.ActivateTabRelative(-1),
+    },
+
+    -- resize mode
+    {
+        key = 'r',
+        mods = 'LEADER',
+        action = act.ActivateKeyTable({
+            name = 'resize_pane',
+            one_shot = false,
+            prevent_fallback = false,
+            replace_current = false,
+            until_unknown = false,
+        }),
+    },
+
+    -- resize pane directly
+    { key = 'H', mods = 'LEADER', action = act.AdjustPaneSize({ 'Left', 3 }) },
+    { key = 'L', mods = 'LEADER', action = act.AdjustPaneSize({ 'Right', 3 }) },
+    { key = 'K', mods = 'LEADER', action = act.AdjustPaneSize({ 'Up', 3 }) },
+    { key = 'J', mods = 'LEADER', action = act.AdjustPaneSize({ 'Down', 3 }) },
+
+    -- change pane mode
+    {
+        key = 'a',
+        mods = 'LEADER',
+        action = act.ActivateKeyTable({
+            name = 'activate_pane',
+            one_shot = true,
+            prevent_fallback = false,
+            replace_current = false,
+            timeout_milliseconds = 1000,
+            until_unknown = false,
+        }),
+    },
+
+    -- change pane directly
+    { key = 'LeftArrow', mods = 'ALT', action = act.ActivatePaneDirection('Left') },
+    { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection('Left') },
+
+    { key = 'RightArrow', mods = 'ALT', action = act.ActivatePaneDirection('Right') },
+    { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection('Right') },
+
+    { key = 'UpArrow', mods = 'ALT', action = act.ActivatePaneDirection('Up') },
+    { key = 'k', mods = 'ALT', action = act.ActivatePaneDirection('Up') },
+
+    { key = 'DownArrow', mods = 'ALT', action = act.ActivatePaneDirection('Down') },
+    { key = 'j', mods = 'ALT', action = act.ActivatePaneDirection('Down') },
+}
+
+-- key tables
+config.key_tables = {
+    resize_pane = {
+        { key = 'LeftArrow', action = act.AdjustPaneSize({ 'Left', 1 }) },
+        { key = 'h', action = act.AdjustPaneSize({ 'Left', 1 }) },
+
+        { key = 'RightArrow', action = act.AdjustPaneSize({ 'Right', 1 }) },
+        { key = 'l', action = act.AdjustPaneSize({ 'Right', 1 }) },
+
+        { key = 'UpArrow', action = act.AdjustPaneSize({ 'Up', 1 }) },
+        { key = 'k', action = act.AdjustPaneSize({ 'Up', 1 }) },
+
+        { key = 'DownArrow', action = act.AdjustPaneSize({ 'Down', 1 }) },
+        { key = 'j', action = act.AdjustPaneSize({ 'Down', 1 }) },
+
+        -- Cancel the mode by pressing escape
+        { key = 'Escape', action = 'PopKeyTable' },
+    },
+
+    activate_pane = {
+        { key = 'LeftArrow', action = act.ActivatePaneDirection('Left') },
+        { key = 'h', action = act.ActivatePaneDirection('Left') },
+
+        { key = 'RightArrow', action = act.ActivatePaneDirection('Right') },
+        { key = 'l', action = act.ActivatePaneDirection('Right') },
+
+        { key = 'UpArrow', action = act.ActivatePaneDirection('Up') },
+        { key = 'k', action = act.ActivatePaneDirection('Up') },
+
+        { key = 'DownArrow', action = act.ActivatePaneDirection('Down') },
+        { key = 'j', action = act.ActivatePaneDirection('Down') },
+    },
+}
+
+-- integration with neovim
+local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
+smart_splits.apply_to_config(config, {
+    direction_keys = { 'h', 'j', 'k', 'l' },
+    modifiers = {
+        move = 'META', -- modifier to use for pane movement, e.g. CTRL+h to move left
+        resize = 'SHIFT', -- modifier to use for pane resize, e.g. META+h to resize to the left
+    },
+    log_level = 'info',
+})
+
+return config
